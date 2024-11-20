aaab3c374f1e3a9fe34a5b51f2606ce703703f5c
Igor <igorlfs@ufmg.br>
2024-11-20 16:40:55 -0300

    faster ucs by applying path rebuild at the end

--- src/ucs.cpp
+++ src/ucs.cpp
@@ -3,12 +3,13 @@
 Path ucs(matrix<double> &M, coords &init, coords &goal) {
     priority_queue<State, vector<State>, greater<>> pq;
     set<coords> visited;
+    matrix<double> cost(W, vector<double>(H, INF));
+    matrix<coords> parent(W, vector<coords>(H, {-1, -1}));
 
-    pq.push({0.0, {init, {init}}});
+    pq.push({0.0, {init}});
 
     while (!pq.empty()) {
-        auto [current_cost, state] = pq.top();
-        auto [current_coords, current_path] = state;
+        auto [current_cost, current_coords] = pq.top();
         pq.pop();
 
         if (visited.count(current_coords) != 0) {
@@ -17,7 +18,9 @@ Path ucs(matrix<double> &M, coords &init, coords &goal) {
         visited.insert(current_coords);
 
         if (current_coords == goal) {
-            return {current_path, current_cost};
+            vector<coords> path = rebuild_path(goal, init, parent);
+
+            return make_pair(path, current_cost);
         }
 
         int x = current_coords.fi;
@@ -29,15 +32,15 @@ Path ucs(matrix<double> &M, coords &init, coords &goal) {
 
             if (is_within_bounds(new_x, new_y) && M.at(new_x).at(new_y) != INF) {
                 double new_cost = current_cost + M.at(new_x).at(new_y);
-                vector<coords> new_path = current_path;
 
-                new_path.emplace_back(new_x, new_y);
-
-                pq.push({new_cost, {{new_x, new_y}, new_path}});
+                if (new_cost < cost[new_x][new_y]) {
+                    cost[new_x][new_y] = new_cost;
+                    parent[new_x][new_y] = {x, y};
+                    pq.emplace(new_cost, make_pair(new_x, new_y));
+                }
             }
         }
     }
 
-    cerr << "A path should always exist\n";
-    exit(1);
+    throw runtime_error(NO_PATH_FOUND_MESSAGE);
 }
